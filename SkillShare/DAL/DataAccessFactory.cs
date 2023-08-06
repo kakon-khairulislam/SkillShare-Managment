5c374ca11dd79135ab6e97025b58392249d4dc14
RaDiO-AcTiVe8970 <62706964+RaDiO-AcTiVe8970@users.noreply.github.com>
2023-08-06 18:40:41 +0600

    InitCommit

---
+++ SkillShare/DAL/DataAccessFactory.cs
@@ -0,0 +1,16 @@
+﻿using DAL.EF.Model;
+using DAL.Interface;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        
+    }
+}
---
+++ SkillShare/DAL/EF/SkillShareContext.cs
@@ -0,0 +1,18 @@
+﻿using DAL.EF.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class ProjectManagementContext: DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project_Members> Project_Members { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+    }
+}
