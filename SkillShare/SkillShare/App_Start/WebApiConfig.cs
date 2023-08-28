4560cda434696dc8310ccae098209530e0d03aa5
RaDiO-AcTiVe8970 <62706964+RaDiO-AcTiVe8970@users.noreply.github.com>
2023-08-29 01:08:36 +0600

    Frontend ADD

--- SkillShare/SkillShare/App_Start/WebApiConfig.cs
+++ SkillShare/SkillShare/App_Start/WebApiConfig.cs
@@ -19,6 +19,7 @@ namespace SkillShare
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.EnableCors(); // Add this line
         }
     }
 }
