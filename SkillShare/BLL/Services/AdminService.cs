b187c3af3ecf24bda256b7cea96c59b716238e01
kakon-khairulislam <kakon190299@gmail.com>
2023-08-29 06:17:06 +0600

    admin entity

--- SkillShare/BLL/Services/AdminService.cs
+++ SkillShare/BLL/Services/AdminService.cs
@@ -13,19 +13,26 @@ namespace BLL.Services
     public class AdminService
     {
         public bool CreateAdmin(AdminDTO admin)
+        public static List<AdminDTO> GetAllAdmin()
         {
+            var data = DataAccessFactory.AdminDataAccess().GetALL();
             var config = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<AdminDTO, Admin>();
+                cfg.CreateMap<Admin, AdminDTO>();
             });
             var mapper = new Mapper(config);
             var insDTO = mapper.Map<Admin>(admin);
             return DataAccessFactory.AdminDataAccess().Create(insDTO);
+            var adDTO = mapper.Map<List<AdminDTO>>(data);
+            return adDTO;
         }
 
         public List<AdminDTO> GetAllAdmin()
+        public static AdminDTO GetAdmin(int id)
         {
             DataAccessFactory.AdminDataAccess().GetALL();
+            var data = DataAccessFactory.AdminDataAccess().Get(id);
             var config = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<Admin, AdminDTO>();
@@ -33,6 +40,36 @@ namespace BLL.Services
             var mapper = new Mapper(config);
             var insDTO = mapper.Map<AdminDTO>(DataAccessFactory.AdminDataAccess.);
             return DataAccessFactory.AdminDataAccess().Create(insDTO);
+            var adDTO = mapper.Map<AdminDTO>(data);
+            return adDTO;
         }
+        public static bool CreateAdmin(AdminDTO ad)
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<AdminDTO, Admin>();
+            });
+            var mapper = new Mapper(config);
+            var adDTO = mapper.Map<Admin>(ad);
+            var data = DataAccessFactory.AdminDataAccess().Create(adDTO);
+            return data;
+        }
+        public static bool UpdateAdmin(AdminDTO ad)
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<AdminDTO, Admin>();
+            });
+            var mapper = new Mapper(config);
+            var adDTO = mapper.Map<Admin>(ad);
+            var data = DataAccessFactory.AdminDataAccess().Update(adDTO);
+            return data;
+        }
+        public static bool DeleteAdmin(int id)
+        {
+            var data = DataAccessFactory.AdminDataAccess().Delete(id);
+            return data;
+        }
+        
     }
 }
